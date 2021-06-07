9e9c1a7ba2a30d0142084a18a1bd09134a98bcdd
Михаил Мясников <gutufik@mail.ru>
2021-06-07 14:45:43 +0300

    Add project files.

---
+++ HolidaysAPI/HolidaysLib.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaysAPI
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Requests
+    {
+        public int used { get; set; }
+        public int available { get; set; }
+        public string resets { get; set; }
+    }
+
+    public class Date
+    {
+        public string name { get; set; }
+        public string numeric { get; set; }
+    }
+
+    public class Observed
+    {
+        public string name { get; set; }
+        public string numeric { get; set; }
+    }
+
+    public class Weekday
+    {
+        public Date date { get; set; }
+        public Observed observed { get; set; }
+    }
+
+    public class Holiday
+    {
+        public string name { get; set; }
+        public string date { get; set; }
+        public string observed { get; set; }
+        public bool @public { get; set; }
+        public string country { get; set; }
+        public string uuid { get; set; }
+        public Weekday weekday { get; set; }
+    }
+
+    public class Root
+    {
+        public int status { get; set; }
+        public string warning { get; set; }
+        public Requests requests { get; set; }
+        public List<Holiday> holidays { get; set; }
+    }
+
+
+}
---
+++ HolidaysAPI/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace HolidaysAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ApiKey = "";
+            string country = "RU";
+            string year = "2020";
+            
+            var url = $"https://holidayapi.com/v1/holidays?pretty&country={country}&year={year}&key={ApiKey}";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                var holidays = JsonConvert.DeserializeObject<Root>(result);
+                foreach (Holiday hd in holidays.holidays)
+                {
+                    Console.WriteLine($"{hd.date} {hd.name}");
+                }
+                
+            }
+
+        }
+    }
+}
